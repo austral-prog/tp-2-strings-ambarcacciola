1cd8432d6cd8178e87af9c04280bf477ecd77364
ambarcacciola <acacciola@mail.austral.edu.ar>
2026-05-21 12:30:58 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,5 +1,32 @@
 def change():
-    """Lee un gasto y el dinero recibido, calcula el vuelto
-    y lo separa en pesos (parte entera) y centavos.
-    """
-    pass
+    print("Ingresar gasto:")
+    gasto_str = input()
+
+    if "." in gasto_str and gasto_str.endswith("0"):
+        gasto_str = gasto_str.rstrip("0")
+
+    print(gasto_str)
+    gasto = float(gasto_str)
+
+
+    print("Dinero recibido")
+    dinero_str = input()
+    print(dinero_str)
+    dinero_recibido = int(dinero_str)
+
+
+    vuelto_total = dinero_recibido - gasto
+    pesos = int(vuelto_total)
+    centavos = round((vuelto_total - pesos) * 100)
+
+
+    print()
+    print("Vuelto")
+    print()
+    print("Pesos:")
+    print(pesos)
+    print("Centavos:")
+    print(centavos)
+
+    if __name__ == "__main__":
+        change()
