6ed3bde847b946b2f7b5278f672a268ac0bb18ef
ambarcacciola <acacciola@mail.austral.edu.ar>
2026-05-22 05:59:01 -0300

    Update names.py

--- names.py
+++ names.py
@@ -1,11 +1,19 @@
 def names():
-    nombre = input("Ingrese su nombre: ")
-    apellido = input("Ingrese su apellido: ")
+    nombre = input("Ingrese su nombre: ").strip()
+    apellido = input("Ingrese su apellido: ").strip()
+
     nombre_completo = nombre + " " + apellido
+
     minusculas = nombre_completo.lower()
     titulo = nombre_completo.title()
     mayusculas = nombre_completo.upper()
     tabulado = "\t" + minusculas
 
-    if __name__ == "__main__":
-        names()
+    print(minusculas)
+    print(titulo)
+    print(mayusculas)
+    print(tabulado)
+
+
+if __name__ == "__main__":
+    names()
