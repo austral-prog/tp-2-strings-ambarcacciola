529e5796e3b1460205975c6ec5f2b9fba6ef4f6e
ambarcacciola <acacciola@mail.austral.edu.ar>
2026-05-22 05:43:14 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,5 +1,9 @@
 def slice_simple():
-    """Dado el texto 'Awesome', imprime distintos substrings
-    usando slicing y lower().
-    """
     texto = "Awesome"
+    texto = texto.lower()
+    print(texto[:3])
+    print(texto[2:5])
+    print(texto)
+
+if __name__ == "__main__":
+    slice_simple()
